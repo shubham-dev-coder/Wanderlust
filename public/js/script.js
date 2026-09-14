53d8192c22ef83a5ad316dc38a14578b41a26156
Shubham Singh <sshubham6508@gmail.com>
2026-09-14 19:12:18 +0530

    Update navbar and listings UI

--- public/js/script.js
+++ public/js/script.js
@@ -18,3 +18,16 @@
         }, false);
     });
 })();
+
+let taxSwitch = document.getElementById("switchCheckDefault");
+taxSwitch.addEventListener("click",()=>{
+    let taxInfo = document.getElementsByClassName("tax-info");
+    for(info of taxInfo){
+        if(info.style.display != "inline"){
+             info.style.display = "inline";
+        }else{
+              info.style.display = "none";
+        }
+       
+    }
+});
